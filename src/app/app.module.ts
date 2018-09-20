3fee68bf53027fcb95f09bbb33afa50f5aabb5e2
Brian Holzschuh <bholzschuh95@gmail.com>
2018-09-20 18:22:52 -0400

    created a login page, register page and imported agularfireauth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,46 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { HttpModule } from '@angular/http';
+import { MyApp } from './app.component';
 
 //page imports
-import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 //service imports
 import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
 
 //firebase imports
-import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+//firebase config import
 import { FIREBASE_CONFIG } from './app.firebase.config';
+
 
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
   ],
   imports: [
     BrowserModule,
     HttpModule,
+
     AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
+
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
   ],
   providers: [
     StatusBar,
---
+++ src/pages/register/register.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { User } from '../../models/user';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  user = {} as User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  register() {
+
+  }
+
+}
