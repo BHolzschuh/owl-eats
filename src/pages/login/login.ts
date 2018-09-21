e5520efc3ccdb3d3c0cced7f272e1428fd5610c3
Brian Holzschuh <bholzschuh95@gmail.com>
2018-09-20 23:56:39 -0400

    quality of life changes

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,11 +14,13 @@ export class LoginPage {
 
   user = {} as User;
   message;
+
+  //constructs all necessary objects
   constructor(private afAuth: AngularFireAuth,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
-
+  //tries to log the user into the system
   async login(user: User) {
     try {
       const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password)
@@ -29,10 +31,22 @@ export class LoginPage {
     }
     catch (e) {
       console.error(e);
-      this.message = e;
+      this.checkErrors(e.code);
     }
   }
 
+  //compares error code with possible throws
+  checkErrors(error: String) {
+    if (error == "auth/invalid-email") {
+      this.message = "Please enter a valid email";
+    }
+    else if (error == "auth/user-not-found") {
+      this.message = "Incorrect email/password";
+    }
+    else if (error == "auth/argument-error") {
+      this.message = "Please fill out both fields";
+    }
+  }
 
   register() {
     this.navCtrl.push('RegisterPage');
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
 
-import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from "angularfire2/auth";
 import { HomePage } from '../home/home';
 
+//class to store the attributes of table entry
 export class Entry {
   uid: string;
   first: string;
@@ -21,12 +22,15 @@ export class RegisterPage {
 
   user = {} as User;
   entry = {} as Entry;
-  message;
+  emessage = '';
+  pmessage = '';
 
+  //constructs all necessary objects
   constructor(private afAuth: AngularFireAuth, private afData: AngularFireDatabase,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  //registers a user into the system
   async register(user: User) {
     try {
       const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
@@ -35,20 +39,38 @@ export class RegisterPage {
         //add user id and names to database
         this.createEntry(this.user.first, this.user.last, result.uid)
 
+        //redirect the page on success
         this.navCtrl.setRoot(HomePage)
       }
     }
+    //if an error tell the user whats wrong
     catch (e) {
       console.error(e);
-      this.message = e;
+      this.checkErrors(e.code);
     }
   }
 
+  //creates an entry for the userInfo table
   createEntry(first: string, last: string, uid: string) {
     this.entry.first = first;
     this.entry.last = last;
     this.entry.uid = uid;
     this.afData.list('/userInfo/').push(this.entry)
+  }
+
+  checkErrors(error: String) {
+    if (error == "auth/invalid-email") {
+      this.emessage = "Please enter a valid email"
+      this.pmessage = '';
+    }
+    else if (error == "auth/weak-password") {
+      this.pmessage = "Password must be at least 6 characters"
+      this.emessage = '';
+    }
+    else if (error == "auth/argument-error") {
+      this.emessage = "Please fill out all fields"
+      this.pmessage = '';
+    }
   }
 
 }
